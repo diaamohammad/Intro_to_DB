c3ef26a1f907735adc1f4c7b55a0170d02e9624a
diaa mohammad <diaamohammad078@gmail.com>
2024-10-20 23:52:43 +0300

    database connection

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,24 +1,35 @@
 import mysql.connector
+from mysql.connector import Error
 
-try:
+def create_database():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='Alsemary55@@'
+        )
 
- connection = mysql.connector.Connect(
-    host="localhost",
-    user="root",
-    password="Alsemary55@@"
- )
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("SHOW DATABASES LIKE 'alx_book_store';")
+            result = cursor.fetchone()
 
- cursor=connection.cursor()
- cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
- print("Database 'alx_book_store' created successfully!")
-except mysql.connector.Error as err:
- print(f"error {err}")
- 
-finally:
- if connection.is_connected:
-  cursor.close()
-  connection.close()
-  print("MySQL connection is closed.")
+            if result:
+                print("Database 'alx_book_store' already exists!")
+            else:
+                cursor.execute("CREATE DATABASE alx_book_store;")
+                print("Database 'alx_book_store' created successfully!")
+
+    except Error as err:
+        print(f"Error: {err}")
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed.")
+
+create_database()
 
 
 
