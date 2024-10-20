9acf4c5b053c21c69998bec85bb8bb6205784d59
diaa mohammad <diaamohammad078@gmail.com>
2024-10-21 00:38:52 +0300

    useing select

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,11 @@
 use alx_book_store;
 
-SHOW COLUMNS FROM books;
+-- This SQL script retrieves the full description of the 'books' table in the 'alx_book_store' database.
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' 
+AND TABLE_NAME = 'books';
+
 
 
  
